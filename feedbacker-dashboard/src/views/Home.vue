8d0acea2456c63210655e4af92d19c34fcdddc29
“JulianySavazzi” <julianysaz@gmail.com>
2024-02-07 19:22:06 -0300

    creating components for home page

--- feedbacker-dashboard/src/views/Home.vue
+++ feedbacker-dashboard/src/views/Home.vue
@@ -1,10 +1,49 @@
+<template>
+  <HomeHeader
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <main>
+    <h1 class="font-black text-brand-main">Home</h1>
+    <Contact/>
+  </main>
+  <Footer/>
+</template>
+
 <script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
+import HomeHeader from "@/components/HomeHeader.vue";
+import Footer from "@/components/Footer.vue";
+import Contact from "@/components/Contact.vue"
+import {useRouter} from 'vue-router'
 </script>
 
-<template>
-  <main>
-    <h1>Home</h1> 
-  </main>
-  
-</template>
+<script>
+export default {
+  //composition api
+  setup(){
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if(token){
+        //se o token nao for vazio, usuario autenticado
+        //podemos acessar a rota feedback
+        router.push({name: 'Feedback'})
+      }
+    })
+
+    function handleLogin(){
+    }
+
+    function handleAccountCreate() {
+
+	  }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+
+  }
+}
+</script>
--- feedbacker-dashboard/tailwind.config.js
+++ feedbacker-dashboard/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
   theme: {
-    extend: {},
+    extend: {
+		// eslint-disable-next-line no-undef
+      colors: require('./palette.js')
+    },
+    fontFamily:{
+      regular: ['RobotoRegular'],
+      medium: ['RobotoMedium'],
+      bold: ['RobotoBold'],
+      black: ['RobotoBlack']
+    }
   },
   plugins: [],
 }
