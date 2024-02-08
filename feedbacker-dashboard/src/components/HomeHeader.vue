7771dc0ec172ac228f206f743568547d750ff025
“JulianySavazzi” <julianysaz@gmail.com>
2024-02-08 20:45:59 -0300

    code refatoring

--- feedbacker-dashboard/src/components/HomeHeader.vue
+++ feedbacker-dashboard/src/components/HomeHeader.vue
@@ -16,24 +16,6 @@
 				</button>
 			</div>
 		</div>
-		<!--<div class="flex flex-col mt-28 max-w-fit">-->
-		<!--&lt;!&ndash;banner items&ndash;&gt;-->
-		<!--	<h1 class="text-4xl font-black text-white">-->
-		<!--		Tenha um feedback.-->
-		<!--		<br>-->
-		<!--		E faça seus clientes mais-->
-		<!--		<br class="hidden lg-inline-block">-->
-		<!--		felizes!-->
-		<!--	</h1>-->
-		<!--	<p class="text-lg font-medium text-white">-->
-		<!--		Receba ideias, reclamações e feedbacks com um-->
-		<!--		<br class="hidden lg-inline-block">-->
-		<!--		simples widget na página.-->
-		<!--	</p>-->
-		<!--	<button class="px-6 py-2 font-bold bg-white rounded-full text-brand-main mt-10 focus:outline:none max-w-fit">-->
-		<!--		Crie uma conta grátis-->
-		<!--	</button>-->
-		<!--</div>-->
 	</div>
 </header>
 </template>
@@ -53,14 +35,4 @@ export default {
 .header-group{
 	@apply flex flex-col w-4/5 max-w-6xl;
 }
-
-/*@media (min-width: 640px) {*/
-/*	.header-group{*/
-/*		background-image: url("../assets/images/home_image.png");*/
-/*		background-size: 622px;*/
-/*		background-position: 90% 100%;*/
-/*		background-repeat: no-repeat;*/
-/*	}*/
-/*}*/
-
 </style>
