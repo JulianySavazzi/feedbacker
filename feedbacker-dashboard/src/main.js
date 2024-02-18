6ffe184931e197c2a5448ca368ec1ccebd8c5835
“JulianySavazzi” <julianysaz@gmail.com>
2024-02-18 10:01:58 -0300

    configuring toasts for error handling

--- feedbacker-dashboard/src/main.js
+++ feedbacker-dashboard/src/main.js
@@ -1,12 +1,19 @@
 import './assets/main.css'
 import './assets/fonts.css'
 import 'animate.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from 'vue-toastification'
 
 const app = createApp(App)
 
+const options = {
+	// You can set your default options here
+};
+
+app.use(Toast, options)
 app.use(router)
 app.mount('#app')
