d79c41f1aa5d50e7e70da6b45f6d7b174a5ef9f7
“JulianySavazzi” <julianysaz@gmail.com>
2024-02-23 19:26:46 -0300

    add and configure sanctum for api autenthication and tokens for login

--- laravel-feedbacker/app/Http/Kernel.php
+++ laravel-feedbacker/app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            //ativando o Sanctum
+             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
