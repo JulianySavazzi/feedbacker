a37045e6b4bb55a64d63b3f0d92495ca7c5d80ea
“JulianySavazzi” <julianysaz@gmail.com>
2024-04-04 19:17:18 -0300

    update header

--- nuxt-feedbacker/components/Header.vue
+++ nuxt-feedbacker/components/Header.vue
@@ -53,8 +53,8 @@ async function handleLogout(){
 							</li>
 							<li>
 								<button
-									class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline:none">
-									{{ auth.user.name }} <span @click="handleLogout" ><Nuxtlink to="/">( SAIR )</Nuxtlink></span>
+									class="px-6 py-2 font-bold bg-white rounded-full text-brand-main ">
+									{{ auth.user.name }} <span @click="handleLogout" class="focus:outline:none bg-brand-main rounded-full text-white" ><Nuxtlink to="/">  ( SAIR )  </Nuxtlink></span>
 								</button>
 							</li>
 
