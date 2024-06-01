e5b01409903e1ff0cb43dcfef8fd675dd8036984
“JulianySavazzi” <julianysaz@gmail.com>
2024-06-01 08:57:34 -0300

    add init widget

---
+++ widget/public/init.js
@@ -0,0 +1,50 @@
+function init (apikey){
+
+    async function handleLoadWidget(){
+        const page = `${window.location.origin}${window.location.pathname}`
+        const fp = await window.FingerprintJS.load()
+        const fingerprint = await fp.get()
+
+        //URL DO WIDGET FEEDBACKER
+        const WIDGET_URL = `?api_key=${apikey}&page=${page}&fingerprint=${fingerprint.visitorId}`
+        const config = {method: 'GET'}
+        const res = await fetch(`?api_key=${apikey}`)
+
+        if(res.status === 200){
+            //iframe
+            const iframe = document.createElement('iframe')
+            iframe.src = WIDGET_URL
+            iframe.id = 'feedbacker-iframe'
+            iframe.style.position = 'fixed'
+            iframe.style.bottom = '0px'
+            iframe.style.right = '0px'
+            iframe.style.overflow = 'hidden'
+            iframe.style.border = '0px'
+            iframe.style.zIndex = '99999'
+            document.body.appendChild(iframe)
+
+            window.addEventListener('message', (event) => {
+              if(!event.data.isWidget) return
+              if(event.data.isOpen){
+                  iframe.width = '100%'
+                  iframe.height = '100%'
+              } else {
+                  iframe.width = '300px'
+                  iframe.height = '150px'
+              }
+            })
+            return
+        } else {
+            console.log('* [feedbacker] was not loaded because apikey does not exists')
+        }
+    }
+
+    const script = document.createElement('script')
+    script.src = 'https://openfpcdn.io/fingerprintjs/v4'
+    script.async = 'async'
+    script.addEventListener('load', handleLoadWidget)
+
+    document.body.appendChild(script)
+}
+
+window.init = init
